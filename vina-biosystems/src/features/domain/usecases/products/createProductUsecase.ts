4d57f7cbd79ab72fe07c7797d21df609fd639f17
joaovitormelo <joao.galo.2014@gmail.com>
2025-06-28 17:21:54 -0300

    Criado caso de uso de 'createProductUsecase'

---
+++ vina-biosystems/src/features/domain/usecases/products/createProductUsecase.ts
@@ -0,0 +1,34 @@
+import { DatabaseException } from "../../../../core/exceptions/databaseException";
+import { ValidationException } from "../../../../core/exceptions/validationException";
+import { ProductsDataContract } from "../../../data/products/productsDataContract";
+import { ProductModel } from "../../models/productModel";
+
+export class CreateProductUsecase {
+    private productsData: ProductsDataContract;
+
+    constructor(productsData: ProductsDataContract) {
+        this.productsData = productsData;
+    }
+
+    async execute(product: ProductModel): Promise<void> {
+        this.validateProductFields(product);
+        try {
+            await this.productsData.createProduct(product);
+        } catch (error) {
+            console.error(error);
+            throw new DatabaseException("Não foi possível criar o produto!");
+        }
+    }
+
+    private validateProductFields(product: ProductModel) {
+        if (!product.getName()) {
+            throw new ValidationException("name", "Nome do produto não pode ser vazio.");
+        }
+        if (product.getQuantity() < 0) {
+            throw new ValidationException("quantity", "Quantidade do produto não pode ser negativa.");
+        }
+        if (!product.getUnit()) {
+            throw new ValidationException("unit", "Unidade do produto não pode ser vazia.");
+        }
+    }
+}
